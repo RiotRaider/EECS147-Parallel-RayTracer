fe9fcc6a4607125437aa2323ffbb0f3346548fe4
RiotRaider <justin.h.sanders93@gmail.com>
2023-02-11 19:33:16 -0800

    All Tests Passed HW5

--- transparent_shader.cpp
+++ transparent_shader.cpp
@@ -23,37 +23,90 @@ vec3 Transparent_Shader::
 {
     // Object Color, Reflected Color, Transmitted Color, Total Color
     vec3 c0, cR, cT, color;
-    //Rays for reflection and refraction anchored at intersection point
+    // Rays for reflection and refraction anchored at intersection point
     Ray reflect, refract;
     reflect.endpoint = intersection_point;
     refract.endpoint = intersection_point;
-    //Find angle of view vector with surface normal
+    // Indexes of Refraction
+    double n1, n2;
+    // Find angle of view vector with surface normal
     double cos1 = dot(-ray.direction, normal);
+    bool enter = (cos1 > 0) ? true : false; // Determine Enter or Exit
+    if (enter)
+    {
+        n1 = 1.0;
+        n2 = index_of_refraction;
+    }
+    else
+    {
+        n1 = index_of_refraction;
+        n2 = 1.0;
+    }
 
-    //Calculate reflected ray and normalize it
+    // Calculate reflected ray and normalize it
     reflect.direction = ((2 * cos1 * normal) + ray.direction).normalized();
 
     // Schlick's Approximation
-    
-        //Recursively cast reflected ray and collect returned color
-        cR = render_world.Cast_Ray(reflect, recursion_depth+1);
-        //PIXEL TRACE
-        Debug_Scope scope;
-        if(Debug_Scope::enable)
-            Pixel_Print("reflected ray: ", reflect,"; reflected color: ", cR);
-        //END PIXEL TRACE
+    double r0 = pow(((n1 - n2) / (n1 + n2)), 2);
+    double R = r0;
 
-        //Find and cast Transmitted Ray
-        
-        color = /*(R * */cR/*)+((1-R)*cT)*/;
-    /*/ PIXEL TRACE
+    // Recursively cast reflected ray and collect returned color
+    cR = render_world.Cast_Ray(reflect, recursion_depth + 1);
+    // PIXEL TRACE
+    Debug_Scope scope;
     if (Debug_Scope::enable)
+        Pixel_Print("reflected ray: ", reflect, "; reflected color: ", cR);
+    // END PIXEL TRACE
+
+    // Find and cast Transmitted Ray
+    double x = n1 / n2;
+    double sini2 = 1 - pow(cos1, 2);
+    if (!enter)
     {
-        Pixel_Print("transmitted ray: ", refract, "; transmitted color: ", cT,"; Schlick Reflectivity: ", R, "; combined color: ", color);
+        cos1 = dot(ray.direction, normal);
+        sini2 = 1 - pow(cos1, 2);
     }
-    */// END PIXEL TRACE
+    if ((x * x * sini2) >= 1)
+    { // complete internal reflection
+
+        // PIXEL TRACE
+        if (Debug_Scope::enable)
+        {
+            Pixel_Print("complete internal reflection;");
+        }
+        // END PIXEL TRACE
+        R = 1;
+    }
+    else
+    {
+        if (enter)
+        {
+            refract.direction = x * ray.direction + normal * (x * cos1 - sqrt(1 - (x * x * sini2)));
+        }
+        else
+        {
+            refract.direction = x * ray.direction - normal * (x * cos1 - sqrt(1 - (x * x * sini2)));
+        }
+
+        refract.direction = refract.direction.normalized(); // normalize Transmitted Ray direction
+        cT = render_world.Cast_Ray(refract, recursion_depth + 1);
+        // Choose the cos(Theta) to use for Schlick's Approx
+        if(!enter){
+            cos1=dot(refract.direction,normal);
+        }
+        R += (1 - r0) * pow((1 - cos1), 5);
+        // Calculate the reflective/refractive portion of the color
+        color = ((1 - R) * cT);
+        // PIXEL TRACE
+        if (Debug_Scope::enable)
+        {
+            Pixel_Print("transmitted ray: ", refract, "; transmitted color: ", cT, "; Schlick Reflectivity: ", R, "; combined color: ", color);
+        }
+        // END PIXEL TRACE
+    }
+    color +=R*cR;
     //  Gather inherent color of object
-    c0 = shader->Shade_Surface(render_world, ray, hit, intersection_point, normal, recursion_depth);
+    c0 = shader->Shade_Surface(render_world, ray, hit, intersection_point, normal, recursion_depth + 1);
 
     color = (opacity * c0) + (1 - opacity) * color;
     // PIXEL TRACE
